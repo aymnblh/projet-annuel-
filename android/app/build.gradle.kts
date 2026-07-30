08cd5c3bbebee24b020587de553b15e7330eadcd
OneClickDev <dev@oneclick.dz>
2026-07-30 11:25:55 +0200

    Fix: set minSdk to 23 for record_android plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.retec.bi3wechri"
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
